3f2888eaa8bd305ea3217c14d19a75abe926f947
Catherine Smith <catsmith24601@gmail.com>
2021-04-26 16:46:48 -0400

    added tests for Manager and Intern test js files

--- tests/Intern.test.js
+++ tests/Intern.test.js
@@ -1,8 +1,26 @@
-
+// Import Intern class
 const Intern = require('../lib/Intern');
 
-describe('', () => {
-    it('', () => {
-        expect(new Employee().);
-    })
+// describe('', () => {
+//     it('', () => {
+//         expect(new Employee().);
+//     })
+// });
+
+test("Can set school name  via constructor", () => {
+    const testValue = "SchoolName";
+    const e = new Intern("Foo", 1, "test@test.com", testValue);
+    expect(e.school).toBe(testValue);
+});
+
+test("getRole() should return \"Intern\"", () => {
+    const testValue = "Intern";
+    const e = new Intern("Foo", 1, "test@test.com", "SchoolName");
+    expect(e.getRole()).toBe(testValue);
+});
+  
+test("Can get school name via getSchool()", () => {
+    const testValue = "SchoolName";
+    const e = new Intern("Foo", 1, "test@test.com", testValue);
+    expect(e.getSchool()).toBe(testValue);
 });
--- tests/Manager.test.js
+++ tests/Manager.test.js
@@ -1,7 +1,21 @@
+// Import Manager class
 const Manager = require('../lib/Manager');
 
-describe('', () => {
-    it('', () => {
-        expect(new Employee().);
-    })
+// describe('', () => {
+//     it('', () => {
+//         expect(new Employee().);
+//     })
+// });
+
+test("Can set office number via constructor", () => {
+    const testValue = "OfficeNumber";
+    const e = new Manager("Foo", 1, "test@test.com", testValue);
+    expect(e.officeNumber).toBe(testValue);
 });
+
+test("getRole() should return \"Manager\"", () => {
+    const testValue = "Manager";
+    const e = new Manager("Foo", 1, "test@test.com",);
+    expect(e.getRole()).toBe(testValue);
+});
+  
